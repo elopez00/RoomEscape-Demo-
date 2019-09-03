aeda67f37c8eeee64b65eb8398e060398aa85f7b
Eddie Lopez <ejlopez00@gmail.com>
2019-09-02 22:41:31 -0400

    RECharacterAI created with perception of player

--- RoomEscape/Source/MyProject/RE_AIController.cpp
+++ RoomEscape/Source/MyProject/RE_AIController.cpp
@@ -3,3 +3,66 @@
 
 #include "RE_AIController.h"
 
+ARE_AIController::ARE_AIController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("Sight Config"));
+	SetPerceptionComponent(*CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("Perception Component")));
+
+	SightConfig->SightRadius = AISightRadius;
+	SightConfig->LoseSightRadius = AILoseSightRadius;
+	SightConfig->PeripheralVisionAngleDegrees;
+	SightConfig->SetMaxAge(AISightAge);
+
+	// Detects Enemies, Friends and Nuetral
+	SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+	SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
+	SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+
+	// Set sight as dominant sense
+	GetPerceptionComponent()->SetDominantSense(*SightConfig->GetSenseImplementation());
+	
+	// Attatch function that will detect pawn
+	GetPerceptionComponent()->OnPerceptionUpdated.AddDynamic(this, &ARE_AIController::OnPawnDetected);
+	GetPerceptionComponent()->ConfigureSense(*SightConfig);
+}
+
+void ARE_AIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (GetPerceptionComponent() != nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("All Systems Set"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Some Problem has Occured"))
+	}
+}
+
+void ARE_AIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+}
+
+void ARE_AIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+FRotator ARE_AIController::GetControlRotation() const
+{
+	if (GetPawn() == nullptr)
+	{
+		return FRotator(0.f, 0.f, 0.f);
+	}
+
+	return FRotator(0.f, GetPawn()->GetActorRotation().Yaw, 0.f);
+}
+
+void ARE_AIController::OnPawnDetected(const TArray<AActor*>& DetectedPawns)
+{
+
+}
--- RoomEscape/Source/MyProject/RE_AIController.h
+++ RoomEscape/Source/MyProject/RE_AIController.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "GameFramework/Controller.h"
 #include "RE_AIController.generated.h"
 
 /**
@@ -13,5 +16,33 @@ UCLASS()
 class MYPROJECT_API ARE_AIController : public AAIController
 {
 	GENERATED_BODY()
+
+public:
+	ARE_AIController();
 	
+	virtual void BeginPlay() override;
+
+	virtual void OnPossess(APawn* InPawn) override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	virtual FRotator GetControlRotation() const override;
+	
+	UFUNCTION()
+		void OnPawnDetected(const TArray<AActor*>& DetectedPawns);
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
+		float AISightRadius = 500.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
+		float AISightAge = 5.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
+		float AILoseSightRadius = AISightRadius + 50.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
+		float AIFieldOfView = 90.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
+		class UAISenseConfig_Sight* SightConfig;
 };
