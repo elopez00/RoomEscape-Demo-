b7bda147614e0587f3a17eada2ce3467f647c9be
Eddie Lopez <ejlopez00@gmail.com>
2019-09-02 21:22:08 -0400

    RECharacter revamp + creation of RECharacterAI and RE_AIController
    Added static mesh + animations to the default character, and added a different mesh for the projectile
    
    Created the RECharacterAI and its RE_AIController with little to no code

---
+++ RoomEscape/Source/MyProject/RECharacterAI.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RECharacterAI.h"
+
+// Sets default values
+ARECharacterAI::ARECharacterAI()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 600.f, 0.f);
+}
+
+// Called when the game starts or when spawned
+void ARECharacterAI::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARECharacterAI::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ARECharacterAI::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ RoomEscape/Source/MyProject/RE_AIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "RE_AIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API ARE_AIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
